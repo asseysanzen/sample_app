8e159ec56598a18686117d5a7aa080a8f235b81f
asseysanzen <asseysanzen@gmail.com>
2020-04-09 17:02:26 +0900

    [finish] 4-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_054159) do
+ActiveRecord::Schema.define(version: 2020_04_09_073031) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "imaged_id"
+    t.string "image_id"
   end
 
 end
